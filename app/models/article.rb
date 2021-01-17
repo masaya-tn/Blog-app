d8680b05fdeedf61bbe0f9610f9a7e45ce715d00
masaya-tn <uvermasayan@icloud.com>
2021-01-17 17:04:48 +0900

    validation機能

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+
+  private
+
+  def validate_title_not_including_comma
+    errors.add(:title, 'にカンマを含めることはできません') if title&.include?(',')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_152640) do
+ActiveRecord::Schema.define(version: 2021_01_17_071627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
